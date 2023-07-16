be8c4502cb44afe4612b92373db263a512f356c9
Saurabh Shukla <saurabh1258.be21@chitkarauniversity.edu.in>
2023-07-16 15:28:22 +0530

    login-updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 function Login() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <CTA>
+
+        <CTALogoOne src="/images/cta-logo-one.svg" />
+        <SignUp>GET ALL THERE</SignUp>
+        <Description>
+          Get Premier Access to Raya and the Last Dragon for an additional fee
+          with a Disney+ subscription. As of 03/26/21, the price of Disney+
+          and The Disney Bundle will increase by $1.
+        </Description>
+        <CTALogoTwo src="/images/cta-logo-two.png" />
+
+      </CTA>
+    </Container>
+    
+  ) 
 }
 
 export default Login;
@@ -27,3 +43,48 @@ const Container = styled.div`
     opacity: 0.7;
   }
 `;
+
+const CTA = styled.div`
+  max-width: 650px;
+  padding: 80px 40px;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+`
+const CTALogoOne = styled.img`
+  width: 100%;
+  
+ 
+`
+const SignUp = styled.a`
+  width: 100%;
+  background-color: #0063e5;
+  font-weight: bold;
+  padding: 17px 0px;
+  color: #f9f9f9;
+  border-radius: 4px;
+  text-align: center;
+  font-size: 18px;
+  cursor: pointer;
+  transition: all 250ms;
+  letter-spacing: 1.5px;
+  margin-top: 8px;
+  margin-bottom: 12px;
+  &:hover {
+    background-color: #0483ee;
+  }
+
+`
+const Description = styled.p`
+  font-size: 11px;
+  letter-spacing: 1.5px;
+  text-align: center;
+  line-height: 1.5;
+
+`
+const CTALogoTwo = styled.img`
+  width: 90%;
+
+`
